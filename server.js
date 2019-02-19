12001b824e3902a5ce303271f32d2dafc39fc7ab
MasonTang <masontang@aol.com>
2019-02-19 06:22:00 -0600

    fixed bug

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ function closeServer() {
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-    runServer(Test_DATABASE_URL).catch(err => console.error(err));
+    runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = { runServer, app, closeServer };
