e5b2f48c2b0dce9c96f94713458db12b44399424
MasonTang <masontang@aol.com>
2019-02-19 07:00:18 -0600

    fixed dot env

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL
-exports.Test_DATABASE_URL = process.env.Test_DATABASE_URL 
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/gradeIt'
+exports.Test_DATABASE_URL = process.env.Test_DATABASE_URL || 'http://localhost:8080'
 exports.PORT = process.env.PORT || 8080;
