00492f7fed327945bea238998a6e118fecca9cb4
MasonTang <masontang@aol.com>
2019-03-01 09:34:04 -0600

    added screenshots

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'http://localhost:8080'
-exports.Test_DATABASE_URL = process.env.Test_DATABASE_URL || 'http://localhost:8080'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/gradeit'
+exports.Test_DATABASE_URL = process.env.Test_DATABASE_URL || 'mongodb://localhost/gradeit'
 exports.PORT = process.env.PORT || 8080;
