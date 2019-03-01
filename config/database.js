c048a9862d2a7c200d980d74b00a6be97288c2d9
MasonTang <masontang@aol.com>
2019-03-01 09:04:35 -0600

    changed font bug

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/gradeIt'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'http://localhost:8080'
 exports.Test_DATABASE_URL = process.env.Test_DATABASE_URL || 'http://localhost:8080'
 exports.PORT = process.env.PORT || 8080;
