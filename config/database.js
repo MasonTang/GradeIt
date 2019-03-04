6dff3b86e2ff73b382f0c21fd08f537c1870e3a9
MasonTang <masontang@aol.com>
2019-03-04 13:54:07 -0600

    updated local database config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/gradeit'
-exports.Test_DATABASE_URL = process.env.Test_DATABASE_URL || 'mongodb://localhost/gradeit'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/gradeIt'
+exports.Test_DATABASE_URL = process.env.Test_DATABASE_URL || 'mongodb://localhost/test-gradeIt'
 exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
-const {DATABASE_URL, PORT, TEST_DATABASE_URL} = require('./config/database.js');
+const {DATABASE_URL, PORT} = require('./config/database.js');
 const {User} = require('./app/models/user');
 const {Semester} = require('./app/models/semester');
 
